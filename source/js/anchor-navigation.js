e0f51bc1680fe718693d4682ab91ca3dc6dbb8f9
Dmitry Kovalenko <dmitry.kovalenko.mu@gmail.com>
2021-08-13 14:25:33 +0300

    img, bundle, sass, fonts

---
+++ source/js/anchor-navigation.js
@@ -0,0 +1,20 @@
+// (() => {
+//     const anchors = document.querySelectorAll('a[href*="#"]');
+
+//     const smoothScrolling = function() {
+//         for (let anchor of anchors) {
+//           anchor.addEventListener('click', function (e) {
+//             e.preventDefault()
+
+//             const blockID = anchor.getAttribute('href').substr(1);
+
+//             document.getElementById(blockID).scrollIntoView({
+//               behavior: 'smooth',
+//               block: 'start'
+//             });
+//           })
+//         }
+//     };
+
+//     smoothScrolling();
+// })();
